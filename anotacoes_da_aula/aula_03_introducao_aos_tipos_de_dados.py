15ecc4d2c569406ca8f117dce46ba46ea25970c4
Gabriel <211716065+GABRIEL-PR0GRAMAD0R@users.noreply.github.com>
2026-04-02 09:40:45 -0300

    Anotações da aula 03 - função print e strings
    Anotações das simples aulas sobre algumas funções da função print.
    
    Comentários

---
+++ anotacoes_da_aula/aula_03_introducao_aos_tipos_de_dados.py
@@ -0,0 +1,23 @@
+
+#Python é uma linguagem de programação com tipagem dinâmica forte.
+#Com isso, quero dizer que a característica dela já sabe o tipo de
+#informação que estamos passando para ele. 
+#Normlamente, colocamos textos dentro de aspas simples (' ') e aspas duplas (" ").
+#Strings são textos que estão dentro de aspas no Python]
+#Ela serve para nós conseguirmos usar um elemento dentro do print sem prejudicar o andamento da função. 
+#E como fazemos? 
+#→ print( "O jetta é um carro \ "SABOR\ " esportivo");
+
+print("Mensagem com aspas duplas");
+print('Mensagem com aspas simples');
+
+print("Abaixo tem uma mensagem com o caracter de escape: ");
+print("O jetta é um carro \"SABOR\" esportivo");
+
+#Agora, se eu quiser que o símbolo \" blablabla \" apareça vamos usar o #R
+print("Exibindo o caracter de escape: ");
+print(r"BMW 320I é um carro \"SABOR\" esportivo, toma benga do Jetta que é puro plástico.");
+print("aqui colocarmos o \"r\" antes das aspas duplas.");
+print("Então fica assim: print(r\" bla bla bla\");");
+
+#Eu uso ';' no final porque programo em outras linguagens, é uma mania que não posso perder. 
