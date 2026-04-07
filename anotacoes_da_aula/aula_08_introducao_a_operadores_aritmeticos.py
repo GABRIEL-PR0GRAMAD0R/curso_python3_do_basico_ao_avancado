25d27789d857dc34ac195c2f86d24aae995097d4
Gabriel <211716065+GABRIEL-PR0GRAMAD0R@users.noreply.github.com>
2026-04-07 16:51:51 -0300

    Anotações da aula 08 - Introdução aos operadores aritméticos Python
    Nessa aula, nós demos uma boa olhada em como são os operadores aritméticos em Python.
    
    Vimos a sintaxe deles:
    soma = 5 + 5;  #Armazena a soma dos valores.
    subtracao = 5 - 7;  #Armazena a subtração dos valores.
    multiplicacao = 8 * 6;  #Armazena a multiplicação dos valores.
    divisao_inteira = 10 // 2;  #Armazena o resultado inteiro da divisão dos valores.
    divisao = 8 / 4;  #Armazena o resultado da divisão entre os valores.
    exponenciacao = 4 ** 6;  #Armazena os a exponeniação de um valor pelo o outro valor. ~
    modulo = 30 % 4;  #Armazena o resto daivisão entre os dois valores.

---
+++ anotacoes_da_aula/aula_08_introducao_a_operadores_aritmeticos.py
@@ -0,0 +1,41 @@
+"""
+Nessa aula, vamos falar um pouco mais sobre os operadores aritméticos do Pytho.
+Ah, mas o que é issu? 
+FDP, VC FUGIU DA ESCOLA? 
+
+Adição, subtração, multiplicação, divisão e por aí vai....
+Bora lá
+
+OBS: Fazer operações entre números inteiros e flutuantes, gera resultado flutuante.
+Se vc pensar em flutuar, vc deveria se tratar. 
+
+"""
+
+# ------------------------------------------------------------------- #
+# Declaração das variáveis que vamos utilizar no programa:
+
+soma = 5 + 5;  #Armazena a soma dos valores.
+subtracao = 5 - 7;  #Armazena a subtração dos valores.
+multiplicacao = 8 * 6;  #Armazena a multiplicação dos valores.
+divisao_inteira = 10 // 2;  #Armazena o resultado inteiro da divisão dos valores.
+divisao = 8 / 4;  #Armazena o resultado da divisão entre os valores.
+exponenciacao = 4 ** 6;  #Armazena os a exponeniação de um valor pelo o outro valor. ~
+modulo = 30 % 4;  #Armazena o resto daivisão entre os dois valores. 
+# ------------------------------------------------------------------- #
+
+print("Vamos brincar com as operações aritméticas:");
+print("");
+
+print("Adição (SOMA) → 5 + 5 =", soma);
+print("Subtração (MENOS) → 5 - 7 =", subtracao);
+print("Multiplicação (VEZES) → 8 * 6 = ", multiplicacao);
+print("Divisão inteira (Comunismo) → 10 // 2 =", divisao_inteira);
+print("Divisão (Comunismo) → 8 / 4 =", divisao);
+print("Exponenciação (Um valor elevado a eles mesmo a x vezes.) → 8 ** 6 = ", exponenciacao);
+print("Módulo (o resto da divisão) → 30 % 4 =", modulo);
+print("");
+
+print("Acima coloquei exatamente como fazer cada uma das operações!");
+print("Usar o módulo é interessante para calcular muitas coisas:");
+print("Verificar se um número é par, se um número é múltiplo de outro e atc.");
+print();
