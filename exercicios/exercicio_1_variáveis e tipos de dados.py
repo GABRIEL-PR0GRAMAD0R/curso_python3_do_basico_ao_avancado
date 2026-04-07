845f41e7f8e8f95c2c1fdb3201c26d0788fc532d
Gabriel <211716065+GABRIEL-PR0GRAMAD0R@users.noreply.github.com>
2026-04-07 16:03:33 -0300

    Resolução do 1º exercício - variáveis e tipos de dados

---
+++ exercicios/exercicio_1_variáveis e tipos de dados.py
@@ -0,0 +1,24 @@
+"""
+Aqui o professor está pedindo para realizarmos um exercício de fixação.
+Devo criar variávies que exibam dados fictícios para entender a atribuição.
+
+"""
+
+#Declaração de variáveis que vamos utilizar no nosso programa:
+primeiro_nome = "Jão"; #guarda o primeiro nome do usuário.
+sobrenome = "Porqueira"; #guarda o sobrenome do usuário.
+idade_do_usuario = 20; #guarda a idade atual do usuario.
+ano_de_nascimento_do_usuario = 1995; #guarda o ano de nascimento do usuario.
+maioridade_brasileira = idade_do_usuario >= 18; #Se verdadeiro, o usuário é maior de idade no Brasil. Caso contrário, não é.
+altura_do_usuario = 1.82; #guarda a altura do usuário em números flutuantes. 
+
+print("Exercício 1 - Exercícios com variáveis e tipos de dados:");
+print("");
+
+print("Nome:", primeiro_nome);
+print("Sobrenome:", sobrenome);
+print("Idade:", idade_do_usuario);
+print("Ano de nascimento:", ano_de_nascimento_do_usuario);
+print("É maior de idade?", maioridade_brasileira);
+print("Altura em metros:", altura_do_usuario, "cm");
+print("");
