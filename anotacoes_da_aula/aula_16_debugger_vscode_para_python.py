4d02358de426e57092a77ca68289c0806ee3732f
Gabriel <211716065+GABRIEL-PR0GRAMAD0R@users.noreply.github.com>
2026-04-23 17:09:20 -0300

    Anotações da aula 16 - Explorando o Debbuger no VS Code
    Nessa aula vimos uma breve introdução sobre o debuger dentro do visual Studio Code e como utilizar ele da maneira correta.
    
    Nessa aula eu só anotei e pratiquei.
    Se vc ficar com dúvidas no futuro, veja uns vídeos do Youtbe.

---
+++ anotacoes_da_aula/aula_16_debugger_vscode_para_python.py
@@ -0,0 +1,19 @@
+"""
+Nesta aula falamos sobre os interpretadores de Debug do Python.     
+Mas o que é o Debug? É a execução linha a linha do programa.       
+
+Em inglês, Bug  é traduzido como inseto. Entretanto, um BUG é relacionado a problemas.     
+Lembre-se: PROBLEMA == BUG      
+
+Debugar o código é rodar o código linha a linha e aí ir vendo aonde o programa está dando erro para "consertar" a lógica.  
+
+O legal é que você vê como o interpretador lê os códigos, da esquerda pra direita.
+Você pode ver as variáveis ganhando valores... Enfim, vc lê e vê o programa funcionando linha a linha. 
+
+Aí o professor explicou um pouco o que são e como usar os breakpoints. 
+São pontos de onde o interpretador do código linha a linha para, nada depois dele é executado.        
+
+Ele explicou um pouco mais sobre como usar o debugger. Bem simples! 
+Botões e suas funções.
+
+"""
